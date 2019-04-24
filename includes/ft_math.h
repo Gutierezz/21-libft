eb94ac0872901436b71824537e29c34b7ccd2caf
Tilma Troll <ttroll@mi-l4.21-school.ru>
2019-04-24 14:47:36 +0300

    lib not inc in math header

--- includes/ft_math.h
+++ includes/ft_math.h
@@ -1,8 +1,6 @@
 #ifndef FT_MATH_H
  #define FT_MATH_H
 
-# include "libft.h"
-
 # define FT_LN_2 0.693147180559945286226763982995
 # define FT_LN_10 2.302585092994045901093613792909
 # define FT_LN_16 2.772588722239781144907055931980
@@ -14,5 +12,6 @@ double				ft_exp(double x);
 double				ft_ln(double x);
 double				ft_log2(double x);
 double				ft_log10(double x);
+double				ft_log16(double x);
 
 #endif
